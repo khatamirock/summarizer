65fce8975c46d1ffd5db1ff27d7b09ea7ff921bc
ucheckit7@gmail.com <777788667555887777>
2022-07-21 23:51:42 +0600

    slider added

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 
-
+var slider = document.getElementById("slder");
 var doctoSend = document.querySelector('.textdiv .input');
 var btn = document.querySelector('.textdiv .btn');
 var docReceived = document.querySelector('.textdiv2 .input');
@@ -13,10 +13,36 @@ btn.addEventListener('click', () => {
     docReceived.value = doctoSend.value;
     docReceived.disabled = false;
     btn.textContent = 'SUMMARIZED';
+
+
+
+    var data = JSON.stringify({
+        "doc": doctoSend.value,
+        "ratio": slider.value
+
+    });
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+            dat = JSON.parse(this.responseText);
+            docReceived.value = dat['Summery'];
+        }
+    });
+
+    xhr.open("POST", "/sumry");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.send(data);
+
+
 })
 console.debug(xx);
-
-
 
 
 
